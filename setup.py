8d6f94790e8d13d6e8599ba240adb2c7901436ac
Ankit billore <ankitbillore2@gmail.com>
2021-10-21 15:06:51 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+     name = "test jenkins and artifact",
+     version = "1.0.1",
+     auther = "ankit",
+     packages=find_packages(),
+     py_modules
+    
+
+)
+
